f7ba2215a1495e979d3202b69cf3966833512589
Chimezie Iwuanyanwu <mr.chrisopher@yahoo.com>
2018-02-22 16:23:39 -0600

    removed unused headers
    unused since name was changed

--- inc/Sentence_Images.h
+++
@@ -1,7 +0,0 @@
-#ifndef __Sentence_Images_h
-#define __Sentence_Images_h
-
-const unsigned short * getLetter(int index);
-const unsigned short * getNumber(int index);
-
-#endif
--- inc/Sprite_Images.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __Sprite_Images_h
-#define __Sprite_Images_h
-
-struct Sprite_t getSprite(int index);
-struct Item_t getItem(int Index);
-
-#endif
